7b12cff2cd055d9c161c7a7abdb57760998ce291
Marcos Dias da Conceição <ti.marcosdias@gmail.com>
2021-09-22 17:14:34 -0300

    Adiciona CORS

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const cors = require("cors");
 const router = require("./routes");
 const app = express();
 const port = 3000;
 
+app.use(cors());
 app.use(express.json());
 app.use(router);
 
